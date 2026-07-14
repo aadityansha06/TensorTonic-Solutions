0f78a5ab9f36060d221703ebd101a2f8d23260ed
Aadityansha <96714228+aadityansha06@users.noreply.github.com>
2026-07-14 19:38:23 +0530

    Solve relu

--- study-plans/cuda-basics/relu/relu.cu
+++ study-plans/cuda-basics/relu/relu.cu
@@ -1,17 +1,36 @@
 #include <cuda_runtime.h>
 #include<math.h>
+#include<vector_types.h>
 
 __global__ void relu_kernel(const float* input, float* output, int N) {
-
     /* relu(x) = { 0 if x<0
-                 { x if x>0
-    */
+                 { x if x>0 */
+    // converting float to float4 to give alignment of 16 byte
+  const  float4* vec_in = reinterpret_cast<const float4*>(input);
+        float4* vec_ou = reinterpret_cast<float4*>(output);
+   
+    // vectroised limit
+    int N_vec = N/4;
+
+    int stride = blockDim.x * gridDim.x;
+    int tx = blockIdx.x * blockDim.x + threadIdx.x;
     
-    unsigned int tx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tx<N){
-            output[tx] = fmaxf(input[tx],0); // max of input or 0 will go 
-                                                
+    for(int i=tx;i<N_vec;i+=stride){
+        float4 vec = vec_in[tx]; // load 16 byte into vec from vec_in array index = tx
+        vec.x = fmaxf(vec.x,0.0f);
+         vec.y = fmaxf(vec.y,0.0f);
+         vec.z = fmaxf(vec.z,0.0f);
+         vec.w = fmaxf(vec.w,0.0f);
+        vec_ou[tx] = vec; //  store 16 byte into vec_ou from vec at array index = tx
     }
+
+    int remainder = N_vec * 4 + tx;
+        for(int i=remainder;i<N;i+=stride){
+            output[i]= fmaxf(input[i],0.0f);
+        }
+    
+    
+    
 }
 
 extern "C" void solve(const float* input, float* output, int N) {
